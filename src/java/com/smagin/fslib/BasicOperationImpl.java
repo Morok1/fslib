43ff80e4e8eb383c7b2760dc2936aa07be4e0946
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-12-02 22:28:07 +0300

    Interface BasicFileRealizations contains main operation of files and folders

---
+++ src/java/com/smagin/fslib/BasicOperationImpl.java
@@ -0,0 +1,22 @@
+package com.smagin.fslib;
+
+import com.smagin.fslib.api.BasicFileOperations;
+
+import java.io.File;
+
+public class BasicOperationImpl implements BasicFileOperations {
+    @Override
+    public File write(File file) {
+        return null;
+    }
+
+    @Override
+    public File append(File file) {
+        return null;
+    }
+
+    @Override
+    public void remove(File file) {
+
+    }
+}
---
+++ src/java/com/smagin/fslib/api/BasicFileOperations.java
@@ -0,0 +1,9 @@
+package com.smagin.fslib.api;
+
+import java.io.File;
+
+public interface BasicFileOperations {
+    File write(File file);
+    File append(File file);
+    void remove(File file);
+}
