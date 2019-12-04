3dc669a6223c92e1e023ed3cabe1a38b12bb55bc
16681981 <Smagin.E.Niko@sberbank.ru>
2019-12-04 20:05:35 +0300

    add methods to interface

--- src/java/com/smagin/fslib/BasicOperationImpl.java
+++ src/java/com/smagin/fslib/BasicOperationImpl.java
@@ -2,18 +2,29 @@ package com.smagin.fslib;
 
 import com.smagin.fslib.api.BasicFileOperations;
 
-import java.io.File;
+import java.io.*;
 
 public class BasicOperationImpl implements BasicFileOperations {
     private final File container;
 
-    public BasicOperationImpl(File container) {
-        this.container = container;
+    public BasicOperationImpl(String name) {
+        this.container = new File(name);
     }
-
 
     @Override
     public File write(File file) {
+        try {
+            FileWriter fstream = new FileWriter(file);
+            BufferedWriter out  = new BufferedWriter(fstream);
+
+            FileInputStream fileInputStream = new FileInputStream(file);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fileInputStream));
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File with cause " + e.getCause());
+        } catch (IOException e) {
+            System.err.println("File operations break with cause" + e.getCause());
+        }
 
         return null;
     }
@@ -24,7 +35,11 @@ public class BasicOperationImpl implements BasicFileOperations {
     }
 
     @Override
-    public void remove(File file) {
+    public void remove(File file) { }
 
-    }
+    @Override
+    public File rename(File file) { return null; }
+
+    @Override
+    public File move(File file) { return null; }
 }
--- src/java/com/smagin/fslib/api/BasicFileOperations.java
+++ src/java/com/smagin/fslib/api/BasicFileOperations.java
@@ -6,4 +6,6 @@ public interface BasicFileOperations {
     File write(File file);
     File append(File file);
     void remove(File file);
+    File rename(File file);
+    File move(File file);
 }
